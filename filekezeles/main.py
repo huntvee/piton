06d03a606b3a60f5fe6a4edcb2d66636ab8ff8fa
huntvee <telotest@gmail.com>
2024-03-01 19:55:11 +0100

    korep change

--- filekezeles/main.py
+++ filekezeles/main.py
@@ -1,3 +1,4 @@
+'''
 filename="input.txt"
 
 file=open(filename, "r")
@@ -33,6 +34,37 @@ while True:
         for i, oszl_ert in enumerate(oszlop_osszeg, 1):
             print(f"{i}. oszlop: {oszl_ert}")
     elif valasz == 0:
+        break
+    else:
+        print('Hibás adatbevitel.')
+'''    
+filename="input.txt"
+
+file=open(filename, "r")
+file_lista=file.readlines()
+file.close()
+lista=[]
+
+for i in range(len(file_lista)):
+    _lista=file_lista[i].strip()
+    _lista_parts=_lista.split()
+    _l=[]
+    for j in range(len(_lista_parts)):
+        _l.append(int(_lista_parts[j]))
+    lista.append(_l)
+print (lista)
+while True:
+    valasz = int(input("1. Számok összege \n 2. Számok soronkénti összege \n 3. Kilépés \n"))
+    if valasz == 1:
+        osszeg = 0
+        for i in range(len(lista)):
+            for x in range(len(lista[i])):
+                osszeg += lista[i][x]
+        print (f'A számok osszege: {osszeg}')
+    elif valasz == 2:
+        for i in range(len(lista)):
+            print(f'Az {i+1} sor osszege: {sum(lista[i])}')
+    elif valasz == 3:
         break
     else:
         print('Hibás adatbevitel.')

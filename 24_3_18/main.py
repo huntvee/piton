df9dbb0d7670837dcc8af87cf75adf6fb7e758ff
huntvee <telotest@gmail.com>
2024-03-18 11:40:38 +0100

    adhdha

--- 24_3_18/main.py
+++ 24_3_18/main.py
@@ -1,3 +1,4 @@
+"""
 import sys
 if len(sys.argv) < 2:
     print("Nem adtal meg filenevet")
@@ -10,3 +11,25 @@ else:
     file.close()
     for i in range(len(sorok)):
         print(sorok[i].strip())
+"""
+import sys
+import os
+## Adatok kivétele a py argument után
+'''
+print("Hello ", end="")
+print(sys.argv[1])
+'''
+
+## Fájl beolvasás
+if len(sys.argv) <= 1:
+    print("Nem érkezett adat!")
+    print("Usage: py filename.py filename.txt")
+elif os.path.exists(sys.argv[1]):
+    filename = sys.argv[1]
+    file = open(filename, encoding="utf-8")
+    rows = file.readlines()
+    file.close
+    for i in range(len(rows)):
+        print(f"Hello {rows[i]}")
+else:
+    print("Nem létezik ilyen fájl.")
